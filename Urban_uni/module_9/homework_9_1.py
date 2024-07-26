2d93077111b9a0213fa7248668831b6abbcda716
VKolesnikov <vk.coworking@gmail.com>
2024-07-26 20:59:14 +0300

    did it

---
+++ Urban_uni/module_9/homework_9_1.py
@@ -0,0 +1,15 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for f in functions:
+        try:
+            results[f.__name__] = f(int_list)
+        except Exception as e:
+            results[f.__name__] = str(e)
+
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+print(apply_all_func(['6', 20, 15, 9], len, sum, sorted, max, min))
