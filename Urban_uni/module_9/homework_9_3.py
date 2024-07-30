3b19d04b7363bc73475408fea1b9ba3176644a43
VKolesnikov <vk.coworking@gmail.com>
2024-07-30 13:34:37 +0300

    did it

--- Urban_uni/module_9/homework_9_3.py
+++ Urban_uni/module_9/homework_9_3.py
@@ -3,7 +3,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 
-second_result = (len(first[i]) == len(second[i]) for i in range(len(min(first, second))))
-
+second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
+# range(min(len(first), len(second)))) - выбираем список с минимальной длинной
 print(list(first_result))
 print(list(second_result))
