9cf359284fc482e4c0e8ff819bb3fdfd0cd95c6e
VKolesnikov <vk.coworking@gmail.com>
2024-07-30 22:49:38 +0300

    did it

--- Urban_uni/module_9/homework_9_6.py
+++ Urban_uni/module_9/homework_9_6.py
@@ -1,21 +1,8 @@
-# def all_variants(text):
-#     length = len(text)
-#     for x in range(1, length + 1):
-#         for y in range(length - x + 1):
-#             yield text[y:y + x]
-#
-# # Пример вызова функции и итерации по результатам
-# a = all_variants("abc")
-# for i in a:
-#     print(i)
-"""def all_var(string):
+def all_var(string):
     for length in range(len(string)):
         for index in range(len(string) - length):
-            print(string[index:index+length+1])
+            yield string[index:index+length + 1]
 
-a = all_var('abc')"""
-
-s = 'abc'
-
-print(range(len(s))) # = 0, 1, 2
-print(s[2:4])
+a = all_var('abc')
+for i in a:
+    print(i)
