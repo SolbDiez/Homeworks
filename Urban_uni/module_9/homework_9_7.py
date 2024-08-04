0406e6ae7ece8b3910102a90a5266c4e3d28ad6b
VKolesnikov <vk.coworking@gmail.com>
2024-08-04 13:43:48 +0300

    did it

--- Urban_uni/module_9/homework_9_7.py
+++ Urban_uni/module_9/homework_9_7.py
@@ -1,13 +1,16 @@
 
 def is_prime(func):
     def wrapper(*args):
-        original_result = func(*args)
-        if isinstance(original_result, int):
-            print('Простое число')
-
+        res = func(*args)
+        numbers = (2, 3, 5, 7)
+        if res < 0 or res == 0 or res == 1:
+            return f'{res}: не простое и не составное.'
+        if res in numbers:
+            return f'{res}: Простое число.'
+        elif res % 2 == 0 or res % 3 == 0 or res % 5 == 0 or res % 7 == 0:
+            return f'{res}: Составное число.'
         else:
-            print('Какое то не простое число')
-        return original_result
+            return f'{res}: Простое число.'
     return wrapper
 
 
@@ -15,8 +18,7 @@ def is_prime(func):
 def sum_three(*args):
     return sum(args)
 
-result_1 = sum_three(2, 3, 6)
+result_1 = sum_three(1, 3, 1)
 print(result_1)
 
-result_2 = sum_three(2.5, 4, 6)
-print(result_2)
+
